5c39d7b4a351773b61ef9a382ab6663a6bd6cdf8
Chris Ninman <ctsnackpack@gmail.com>
2022-01-02 12:28:55 +0100

    commit tree 3e4a70a169613e5327275b685d4543dc1091c995

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_string = array.pop
+    array << "and "
+    array.join(", ") + last_string
+  end
+end
+
+puts oxford_comma(["kiwi", "durian"])
+
+def knoxford_comma(array)
+  array[-1] = "and " + array[-1]
+  array.join(", ")
 end
